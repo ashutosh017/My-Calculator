eb487bbc59709fb408a0d71e7ae37920659dc1f2
ashutosh017 <102981372+ashutosh017@users.noreply.github.com>
2022-06-03 20:56:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Welcome to my calculator");
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        if (e.target.innerHTML == 'x') {
+            
+        }
+        else if (e.target.innerHTML == 'C') {
+            string = ""
+            document.querySelector('input').value = string;
+        }
+        else {
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+})
