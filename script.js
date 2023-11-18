cc1929fa80cc2dcd4d1a870f6c0ecc54b74dcbf3
Ashutosh <102981372+ashutosh017@users.noreply.github.com>
2023-11-18 12:14:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ Array.from(buttons).forEach((button) => {
             string = ""
             document.querySelector('input').value = string;
         }
+        else if (e.target.innerHTML == 'x') {
+            string = string.slice(0, string.length - 1);
+            document.querySelector('input').value = string;
+
+        }
         else {
             console.log(e.target)
             string = string + e.target.innerHTML;
